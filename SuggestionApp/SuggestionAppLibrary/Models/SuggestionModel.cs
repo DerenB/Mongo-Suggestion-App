cc112bd2101db11c016bc3711e526bf83cd8a484
Deren B <dbozerprogramming@gmail.com>
2022-02-01 21:18:21 -0500

    Suggestion Model

--- SuggestionApp/SuggestionAppLibrary/Models/SuggestionModel.cs
+++ SuggestionApp/SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -2,4 +2,13 @@
 
 public class SuggestionModel
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Suggestion { get; set; }
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    public CategoryModel Category { get; set; }
+    public string Author { get; set; }
+    public int UserVotes { get; set; }
 }
