0d11fd0d70e7f1782015a3540d531f9aea150717
Deren B <dbozerprogramming@gmail.com>
2022-02-02 13:11:36 -0500

    User Model

---
+++ SuggestionApp/SuggestionAppLibrary/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class UserModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string ObjectIdentifier { get; set; }
+
+}
