0bda99ce6e1486fd296b485c392394a7e25397b2
Deren B <dbozerprogramming@gmail.com>
2022-02-02 13:16:43 -0500

    User Model

---
+++ SuggestionApp/SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class BasicSuggestionModel
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+}
--- SuggestionApp/SuggestionAppLibrary/Models/UserModel.cs
+++ SuggestionApp/SuggestionAppLibrary/Models/UserModel.cs
@@ -6,5 +6,10 @@ public class UserModel
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public string ObjectIdentifier { get; set; }
-
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    public string EmailAddress { get; set; }
+    public List<SuggestionModel> AuthorSuggestions { get; set; } = new();
+    public List<SuggestionModel> VotedOnSuggestions { get; set; } = new();
 }
