b6ab975b0710af95d1085f09b6a1c6e64aec5d44
Deren B <dbozerprogramming@gmail.com>
2022-02-07 19:41:11 -0500

    Video 10 partial

---
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/MongoUserData.cs
@@ -0,0 +1,11 @@
+﻿namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoUserData
+{
+    private readonly IDbConnection db;
+
+    public MongoUserData(IDbConnection db)
+    {
+        this.db = db;
+    }
+}
