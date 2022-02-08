579e236aa6aa7f62b45284761219a897d47b14df
Deren B <dbozerprogramming@gmail.com>
2022-02-08 13:39:51 -0500

    Mongo statuses

---
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoSuggestionData
+{
+    private readonly IDbConnection _db;
+    private readonly IUserData _userData;
+    private readonly Microsoft.Extensions.Caching.Memory.IMemoryCache _cache;
+    private readonly IMongoCollection<SuggestionModel> _suggestions;
+    private const string CacheName = "SuggestionData";
+
+    public MongoSuggestionData(IDbConnection db, IUserData userData, Microsoft.Extensions.Caching.Memory.IMemoryCache cahce)
+    {
+        _db = db;
+        _userData = userData;
+        _cache = cahce;
+        _suggestions = db.SuggestionCollection;
+    }
+
+    public async Task<List<SuggestionModel>> GetAllSuggestions()
+    {
+        var output = _cache.Get<List<SuggestionModel>>(CacheName);
+        if(output is null)
+        {
+            var results = await _suggestions.FindAsync(s => s.Archived == false);
+            output = results.ToList();
+
+            _cache.Set(CacheName, output, TimeSpan.FromMinutes(1));
+        }
+
+        return output;
+    }
+}
