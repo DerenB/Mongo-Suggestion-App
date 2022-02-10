b3e29dc20b083dda812c382151209705bb54b2cb
Deren B <dbozerprogramming@gmail.com>
2022-02-10 13:53:10 -0500

    Register Services

--- SuggestionApp/SuggestionAppUI/RegisterServices.cs
+++ SuggestionApp/SuggestionAppUI/RegisterServices.cs
@@ -1,4 +1,7 @@
-﻿namespace SuggestionAppUI;
+﻿using Microsoft.Extensions.Caching.Memory;
+using SuggestionAppLibrary.DataAccess;
+
+namespace SuggestionAppUI;
 
 public static class RegisterServices
 {
@@ -7,5 +10,11 @@ public static class RegisterServices
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
         builder.Services.AddMemoryCache();
+
+        builder.Services.AddSingleton<IDbConnection, DbConnection>();
+        builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
+        builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+        builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();
+        builder.Services.AddSingleton<IUserData, MongoUserData>();
     }
 }
